997fd8737e3f0982b12fb4173bbede16ce0255a2
unknown <henryarcv2@gmail.com>
2023-10-26 03:15:50 +0800

    change baseURL

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const baseURL = 'http://localhost:3000/api';
-
+//const baseURL = 'http://localhost:3000/api';
+const baseURL = 'ec2-52-11-140-52.us-west-2.compute.amazonaws.com';
 export default axios.create({baseURL});
 
 export const axiosPrivate = axios.create({
